21df7775796a004639ddd534892cfe3cb4276fdd
Gert Verhoog <me@gertalot.com>
2025-10-18 16:49:42 +1300

    feat: add CanvasRenderer trait with associated coordinate type

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,7 +1,9 @@
 pub mod coords;
+pub mod renderer_trait;
 pub mod transforms;
 pub mod viewport;
 
 pub use coords::{ImageCoord, ImageRect, PixelCoord};
+pub use renderer_trait::CanvasRenderer;
 pub use transforms::{calculate_visible_bounds, image_to_pixel, pixel_to_image};
 pub use viewport::Viewport;
---
+++ src/rendering/renderer_trait.rs
@@ -0,0 +1,13 @@
+
+use crate::rendering::coords::ImageRect;
+
+pub trait CanvasRenderer {
+    type Coord: Clone;
+
+    /// Returns the natural bounds of this renderer (what zoom 1.0 should display)
+    fn natural_bounds(&self) -> ImageRect<Self::Coord>;
+
+    /// Renders the specified image-space rectangle to pixel data
+    /// Returns RGBA pixel data (width * height * 4 bytes)
+    fn render(&self, target_rect: &ImageRect<Self::Coord>, width: u32, height: u32) -> Vec<u8>;
+}
