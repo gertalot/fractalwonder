c0e971c53545f3aa3945af8d9184fd3e4d206f82
Gert Verhoog <me@gertalot.com>
2025-10-20 02:29:28 +1300

    feat: add PixelRect type for pixel-space rectangles

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,11 +1,13 @@
 pub mod canvas_utils;
 pub mod coords;
+pub mod pixel_rect;
 pub mod renderer_trait;
 pub mod transforms;
 pub mod viewport;
 
 pub use canvas_utils::render_with_viewport;
 pub use coords::{Coord, Rect};
+pub use pixel_rect::PixelRect;
 pub use renderer_trait::CanvasRenderer;
 pub use transforms::{
     apply_pixel_transform_to_viewport, calculate_aspect_ratio, calculate_visible_bounds,
