b7a74bc1a31cbabd2d48db2eb3f97bcde6ed0ce8
Gert Verhoog <me@gertalot.com>
2025-11-17 17:13:39 +1300

    feat: implement start_render for WorkerPool

--- fractalwonder-ui/src/workers/worker_pool.rs
+++ fractalwonder-ui/src/workers/worker_pool.rs
@@ -1,5 +1,5 @@
-use fractalwonder_compute::WorkerRequest;
-use js_sys::ArrayBuffer;
+use fractalwonder_compute::{SharedBufferLayout, WorkerRequest};
+use fractalwonder_core::Viewport;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 use wasm_bindgen::prelude::*;
@@ -7,8 +7,7 @@ use web_sys::{MessageEvent, Worker};
 
 pub struct WorkerPool {
     workers: Vec<Worker>,
-    #[allow(dead_code)]
-    shared_buffer: Option<ArrayBuffer>,
+    shared_buffer: Option<js_sys::SharedArrayBuffer>,
     current_render_id: Arc<AtomicU32>,
 }
 
@@ -31,10 +30,7 @@ impl WorkerPool {
             // Note: Not using WorkerType::Module because worker uses no-modules target
             let worker = Worker::new("./fractalwonder-compute.js")?;
 
-            web_sys::console::log_1(&JsValue::from_str(&format!(
-                "Worker {} created",
-                i
-            )));
+            web_sys::console::log_1(&JsValue::from_str(&format!("Worker {} created", i)));
 
             // Set up message handler for worker responses
             let worker_id = i;
@@ -42,8 +38,7 @@ impl WorkerPool {
                 if let Some(msg) = e.data().as_string() {
                     web_sys::console::log_1(&JsValue::from_str(&format!(
                         "Worker {} message: {}",
-                        worker_id,
-                        msg
+                        worker_id, msg
                     )));
                 }
             }) as Box<dyn FnMut(_)>);
@@ -79,6 +74,86 @@ impl WorkerPool {
 
     pub fn current_render_id(&self) -> u32 {
         self.current_render_id.load(Ordering::SeqCst)
+    }
+
+    pub fn start_render(
+        &mut self,
+        viewport: &Viewport<f64>,
+        canvas_width: u32,
+        canvas_height: u32,
+        tile_size: u32,
+    ) -> Result<u32, JsValue> {
+        // Increment render ID
+        let render_id = self.current_render_id.fetch_add(1, Ordering::SeqCst) + 1;
+
+        web_sys::console::log_1(&JsValue::from_str(&format!(
+            "Starting render {} ({}x{}, tile_size={})",
+            render_id, canvas_width, canvas_height, tile_size
+        )));
+
+        // Create SharedArrayBuffer layout
+        let layout = SharedBufferLayout::new(canvas_width, canvas_height);
+        let buffer_size = layout.buffer_size();
+
+        web_sys::console::log_1(&JsValue::from_str(&format!(
+            "Creating SharedArrayBuffer of {} bytes",
+            buffer_size
+        )));
+
+        // Create SharedArrayBuffer
+        let shared_buffer = js_sys::SharedArrayBuffer::new(buffer_size as u32);
+        self.shared_buffer = Some(shared_buffer.clone());
+
+        // Initialize atomic counters in buffer
+        let int32_array = js_sys::Int32Array::new(&shared_buffer);
+        int32_array.set_index(0, 0); // tile_index counter = 0
+        int32_array.set_index(1, render_id as i32); // render_id
+
+        // Zero out pixel data
+        let view = js_sys::Uint8Array::new(&shared_buffer);
+        for i in 8..buffer_size {
+            view.set_index(i as u32, 0);
+        }
+
+        // Serialize viewport to JSON
+        let viewport_json = serde_json::to_string(viewport)
+            .map_err(|e| JsValue::from_str(&format!("Serialize viewport error: {}", e)))?;
+
+        // Create render request
+        let request = WorkerRequest::Render {
+            viewport_json,
+            canvas_width,
+            canvas_height,
+            render_id,
+            tile_size,
+        };
+
+        let message = serde_json::to_string(&request)
+            .map_err(|e| JsValue::from_str(&format!("Serialize request error: {}", e)))?;
+
+        // Send to all workers
+        for (i, worker) in self.workers.iter().enumerate() {
+            web_sys::console::log_1(&JsValue::from_str(&format!(
+                "Sending render request to worker {}",
+                i
+            )));
+
+            // Post message with shared buffer
+            // Note: We send the buffer reference, not transfer it
+            worker.post_message(&JsValue::from_str(&message))?;
+        }
+
+        web_sys::console::log_1(&JsValue::from_str(&format!(
+            "Render {} started on {} workers",
+            render_id,
+            self.workers.len()
+        )));
+
+        Ok(render_id)
+    }
+
+    pub fn get_shared_buffer(&self) -> Option<&js_sys::SharedArrayBuffer> {
+        self.shared_buffer.as_ref()
     }
 }
 
