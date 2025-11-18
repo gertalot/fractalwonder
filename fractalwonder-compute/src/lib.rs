6983f614e7ce7d270020173a2d961732ec90b166
Gert Verhoog <me@gertalot.com>
2025-11-19 00:13:03 +1300

    dead compute code gone

--- fractalwonder-compute/src/lib.rs
+++ fractalwonder-compute/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod adaptive_mandelbrot_renderer;
 pub mod app_data_renderer;
-pub mod cancellation;
 pub mod computers;
 pub mod messages;
 pub mod pixel_renderer;
@@ -9,18 +8,12 @@ pub mod precision;
 pub mod render_config;
 pub mod renderer_info;
 pub mod renderer_trait;
-pub mod shared_buffer;
-pub mod worker_messages;
-
-#[cfg(target_arch = "wasm32")]
-pub mod atomics;
 
 #[cfg(target_arch = "wasm32")]
 pub mod worker;
 
 pub use adaptive_mandelbrot_renderer::AdaptiveMandelbrotRenderer;
 pub use app_data_renderer::AppDataRenderer;
-pub use cancellation::{AtomicBoolChecker, CancellationChecker, NeverCancel};
 pub use computers::{MandelbrotComputer, TestImageComputer};
 pub use messages::{MainToWorker, WorkerToMain};
 pub use pixel_renderer::PixelRenderer;
@@ -29,14 +22,9 @@ pub use precision::PrecisionCalculator;
 pub use render_config::{get_color_scheme, get_config, ColorScheme, RenderConfig, RENDER_CONFIGS};
 pub use renderer_info::{RendererInfo, RendererInfoData};
 pub use renderer_trait::Renderer;
-pub use shared_buffer::SharedBufferLayout;
-pub use worker_messages::{WorkerRequest, WorkerResponse};
 
 #[cfg(target_arch = "wasm32")]
-pub use atomics::{atomic_fetch_add_u32, atomic_load_u32, atomic_store_u32};
-
-#[cfg(target_arch = "wasm32")]
-pub use worker::{handle_message, init_worker, process_render_request};
+pub use worker::init_message_worker;
 
 // Re-export core types for convenience
 pub use fractalwonder_core::*;
