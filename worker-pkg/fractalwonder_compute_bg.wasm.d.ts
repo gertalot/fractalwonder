3db202c0513728b22ba3e645093be7f9d050c612
gertalot <gertalot@users.noreply.github.com>
2025-12-02 22:26:05 +0000

    deploy: 3c7518423206ca9dee321c839e106546cd71d85a

---
+++ worker-pkg/fractalwonder_compute_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const init_message_worker: () => void;
+export const wasm_bindgen__convert__closures_____invoke__h76be7d37eab3dc17: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__closure__destroy__hb455058b08ce9d15: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
