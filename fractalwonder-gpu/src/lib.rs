e992e693fd5f50498221ed1f3045bffe9fb1b5b9
Gert Verhoog <me@gertalot.com>
2025-12-22 10:41:00 +1300

    delete unused renderer code

--- fractalwonder-gpu/src/lib.rs
+++ fractalwonder-gpu/src/lib.rs
@@ -3,20 +3,14 @@
 mod buffers;
 mod device;
 mod error;
-mod perturbation_hdr_pipeline;
-mod perturbation_hdr_renderer;
 mod progressive_pipeline;
 mod progressive_renderer;
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests;
 
-pub use buffers::{
-    PerturbationHDRBuffers, PerturbationHDRUniforms, ProgressiveGpuBuffers, ProgressiveGpuUniforms,
-};
+pub use buffers::{ProgressiveGpuBuffers, ProgressiveGpuUniforms};
 pub use device::{GpuAvailability, GpuContext};
 pub use error::GpuError;
-pub use perturbation_hdr_pipeline::PerturbationHDRPipeline;
-pub use perturbation_hdr_renderer::{GpuPerturbationHDRRenderer, GpuTileResult};
 pub use progressive_pipeline::ProgressiveGpuPipeline;
 pub use progressive_renderer::{ProgressiveGpuRenderer, ProgressiveRowSetResult};
 
